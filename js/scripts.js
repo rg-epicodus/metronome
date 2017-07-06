4be235b12301d7dfb76ddc084c4b841d3e387552
Richard Garrick <richg341@gmail.com>
2017-07-06 14:47:27 -0700

    add flashing dot with same interval as counter, add counter function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
-var count = prompt("Please enter your max number:");
+$(document).ready(function() {
+
+  var count = parseInt(prompt("Please enter your max number:"));
+  var bpm = parseInt(prompt("please enter tempo '1000 is 60 beats per minute'"));
+  var colors = ["black", "yellow"];
+
+  var colorsCounter = 0;
+  var interval = setInterval(function() {
+    $(".circleBase").css("background-color", colors[colorsCounter]);
+    for (var i = 0; i < count; i++) {
+      if(colorsCounter === count) {
+        clearInterval(interval);
+      } else if (colorsCounter === 1) {
+        colorsCounter = 0;
+      } else {
+        colorsCounter += 1;
+      }
+    };
+}, bpm); //cant get css to flash when less than 1000 in firefox//
+
 
 for (var i=0;i<=count;i++) {
    (function(tick) {
        setTimeout(function() {
-       $("#ticker").append(tick);
-       }, 1000 * (tick+1) );
+       $("#ticker").text(tick);
+     }, bpm * (tick+1) );
    })(i);
 }
+});
